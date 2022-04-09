fc6aab1969dbe1aeaf7591789ddbbe298321613a
Anand Mahesh <manand881@gmail.com>
2022-04-09 10:54:23 +0530

    final

--- src/Components/Pages/Doctors/Doctors.jsx
+++ src/Components/Pages/Doctors/Doctors.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import style from './Doctors.module.css';
+import {Jumbotron} from './Jumbotron/Jumbotron.jsx';
 
 export const Doctors = () => {
   return (
-    <div>Doctors</div>
+    <div className={style.container}>
+      <Jumbotron/>
+    </div>
   )
 }
---
+++ src/Components/Pages/Doctors/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import './Jumbotron.css'
+
+export const Jumbotron = () => {
+	return (
+		<section>
+			<style scoped>
+				@import url('https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css');
+			</style>
+			<div className="bg-dark-blue">
+				<div className="row">
+					<div className="col align-self-center">
+						<div className="container-fluid ">
+							<div className="jumbotron jumbotron-fluid min-460 imagebackground">
+								<div className="container justify-items-center">
+									<div className="row">
+										<div className="col d-flex justify-content-center">
+											<div className="row">
+												<p className="header-text font">Your home for health</p>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col d-flex justify-content-center">
+											<div className="row">
+												<p className="book-text font mt-5">Find and Book</p>
+											</div>
+										</div>
+									</div>
+									<div className="row justify-content-center">
+										<input type="text"  placeholder="Bangalore" className="p-1 f14 min-50"/>
+											<input type="text"  className="p-1 f14 fit-content min-50"
+												placeholder="Search Doctors, clinics, hospitals"/>
+											</div>
+									</div>
+									<div className="row">
+										<div className="col d-flex justify-content-center">
+											<div className="row mt-2">
+												<div className="col">
+													<p className="font mt-2 f12 grey-font">Popular Searches</p>
+												</div>
+												<div className="col">
+													<p className="font mt-2 f12 grey-font">Dermatolgist</p>
+												</div>
+												<div className="col">
+													<p className="font mt-2 f12 grey-font">Pediatrician</p>
+												</div>
+												<div className="col">
+													<p className="font mt-2 f12 grey-font">Gynecologist</p>
+												</div>
+												<div className="col">
+													<p className="font mt-2 f12 grey-font">Others</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="bg-darkerblue">
+					<div className="row">
+						<div className="col d-flex justify-content-center">
+							<div className="row mt-2 pt-1 pb-1">
+								<div className="col">
+									<p className="font mt-2 consult text-center">Consult With Doctor</p>
+								</div>
+								<div className="col">
+									<p className="font mt-2 consult text-center">Other Medicines</p>
+								</div>
+								<div className="col">
+									<p className="font mt-2 consult text-center">View Medical Records</p>
+								</div>
+								<div className="col">
+									<p className="font mt-2 consult text-center">Book Test</p>
+								</div>
+								<div className="col">
+									<p className="font mt-2 consult text-center">Read Articles</p>
+								</div>
+								<div className="col">
+									<p className="font mt-2 consult text-center">For Healthcare Providers</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="h7000"></div>
+		</section>
+	)
+}
